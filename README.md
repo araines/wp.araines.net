8c8cab6b3f1389ddc4267fc1e312c9d3dfdbdb3b
Andy Raines <andrew.raines@gmail.com>
2024-05-04 12:18:01 +0100

    feat: Get everything working nicely

--- README.md
+++ README.md
@@ -26,33 +26,9 @@ terraform apply
 
 5. Change the admin password
 
-6. Install and activate the `GeneratePass` theme
+6. Customise the theme
 
-7. Customise the theme
-
-   a. The following colours:
-
-   ```
-   contrast:   #222222
-   contrast-2: #bc986a
-   contrast-3: #daad86
-   base:       #fbeec1
-   base-2:     #659dbd
-   base-3:     #ffffff
-   accent:     #7e783a
-   ```
-
-   b. Change "Entry meta text" to `accent`
-
-8. Go to Settings->Permalinks and select Post Name
-
-9. Go to Settings->General and configure timezones, language etc
-
-10. Go to Settings->Discussion and disable everything in "Default post settings"
-
-11. Install and activate `Yoast SEO` plugin (consider Rank Math?)
-
-12. Install and activate `UpdraftPlus` plugin for backups / local dev
+   a. TODO
 
 ## Starting / stopping WordPress
 
@@ -70,35 +46,18 @@ terraform apply
 
 ## Site publication
 
-### First-time Setup
-
-1. Login to `wp-admin`.
-
-2. Go to WP2Static->Addons and enable the S3 deployment addon.
-
-3. Click the settings cog and change the Object ACL to `private`, then Save S3 Options.
-
 ### Normal publication
 
 To publish the site statically, login to `wp-admin`, then go to WP2Static and press Generate Static Site.
 
 ## Accessing WordPress
 
-Go to the WordPress dynamic website at [wordpress.food.araines.net](http://wordpress.food.araines.net).
+Go to the WordPress dynamic website at [wordpress.araines.net](http://wordpress.araines.net).
 
-The admin interface can be accessed by [wp-admin](http://wordpress.food.araines.net/wp-admin).
+The admin interface can be accessed by [wp-admin](http://wordpress.araines.net/wp-admin).
 
 ## Troubleshooting
 
 ### Generate static site: Unable to fetch URL contents
 
 During the static site generation, if it gets a 500 error with the last log being "Unable to fetch URL contents" then try clearing the caches.
-
-## TODO
-
-Configure OIDC to allow GitHub actions to talk to AWS - permissions need doing
-Create an action for building docker container and pushing to ECR
-Update the apache2-endpoint.sh to work in the ECR environment (with correct DNS/IPs etc)
-Update the apache2-endpoint.sh to update R53 so container is accessible
-Set up multisite
-Update the apache2-endpoint.sh to install GP Premium / recipe plugins / backup plugins / etc
--- wordpress/apache2-entrypoint.sh
+++ wordpress/apache2-entrypoint.sh
@@ -28,6 +28,26 @@ fi
 if ! sudo -E -u www-data wp core is-installed; then
   echo "WordPress not installed: installing WordPress"
   sudo -E -u www-data wp core install --url="http://${CONTAINER_DNS}" --title="${WORDPRESS_SITE_NAME}" --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --skip-email
+
+  # UK locale settings
+  sudo -E -u www-data wp language core install --activate en_GB
+  sudo -E -u www-data wp option update timezone_string "Europe/London"
+  sudo -E -u www-data wp option update date_format "j F Y"
+  sudo -E -u www-data wp option update time_format "H:i"
+
+  # Disable pingbacks and commenting (won't work on static site)
+  sudo -E -u www-data wp option update default_pingback_flag 0
+  sudo -E -u www-data wp option update default_ping_status 0
+  sudo -E -u www-data wp option update default_comment_status 0
+
+  # Postname URL structures for permalinks (better for SEO)
+  sudo -E -u www-data wp option update permalink_structure "/%postname%/"
+
+  # Remove default plugins
+  sudo -E -u www-data wp plugin delete akismet hello
+
+  # Remove unwanted default themes
+  sudo -E -u www-data wp theme delete twentytwentytwo twentytwentythree
 fi
 
 chown www-data:www-data /root
@@ -45,20 +65,32 @@ if ! sudo -E -u www-data wp plugin is-installed wp2static; then
   fi
 fi
 
-# Update WordPress options with IP of running container
-sudo -E -u www-data wp option update siteurl "http://${CONTAINER_DNS}"
-sudo -E -u www-data wp option update home "http://${CONTAINER_DNS}"
-
 # Update WP2Static options, if environment variables are set
 if [ -n "${WPSTATIC_DEST-}" ]; then
   sudo -E -u www-data wp wp2static options set deploymentURL "${WPSTATIC_DEST}"
 fi
-if [ -n "${WPSTATIC_REGION-}" ]; then
-  sudo -E -u www-data wp db query "UPDATE wp_wp2static_addon_s3_options SET value = '$WPSTATIC_REGION' WHERE name = 's3Region';"
+if [ -n "${WPSTATIC_REGION-}" ] && [ -n "${WPSTATIC_BUCKET-}" ]; then
+  sudo -E -u www-data wp db query "UPDATE wp_wp2static_addon_s3_options SET value = '$WPSTATIC_REGION' WHERE name = 's3Region'"
+  sudo -E -u www-data wp db query "UPDATE wp_wp2static_addon_s3_options SET value = '$WPSTATIC_BUCKET' WHERE name = 's3Bucket'"
+  sudo -E -u www-data wp db query "UPDATE wp_wp2static_addon_s3_options SET value = 'private' WHERE name = 's3ObjectACL'"
+  sudo -E -u www-data wp db query "UPDATE wp_wp2static_addons SET enabled = 1 WHERE slug = 'wp2static-addon-s3'"
 fi
-if [ -n "${WPSTATIC_BUCKET-}" ]; then
-  sudo -E -u www-data wp db query "UPDATE wp_wp2static_addon_s3_options SET value = '$WPSTATIC_BUCKET' WHERE name = 's3Bucket';"
+
+# Install UpdraftPlus
+if ! sudo -E -u www-data wp plugin is-installed updraftplus; then
+  echo "UpdraftPlus not installed: installing UpdraftPlus"
+  sudo -E -u www-data wp plugin install --activate updraftplus
 fi
+
+# Install Yoast SEO
+if ! sudo -E -u www-data wp plugin is-installed wordpress-seo; then
+  echo "Yoast SEO not installed: installing Yoast SEO"
+  sudo -E -u www-data wp plugin install --activate wordpress-seo
+fi
+
+# Update WordPress options with IP of running container
+sudo -E -u www-data wp option update siteurl "http://${CONTAINER_DNS}"
+sudo -E -u www-data wp option update home "http://${CONTAINER_DNS}"
 
 echo "Starting Apache"
 exec "apache2-foreground"
